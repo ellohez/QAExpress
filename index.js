cea8a90842641a64f8bb8618bcab1d6a4c5900dd
Helen Pickavance <helenpjobs@gmail.com>
2023-04-25 12:12:16 +0100

    Logging and error handling added

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 // import Express to a constant using require()
-const express =require('express');
+const express = require('express');
 // Call that constant as function to initialise the Express application
 const app = express();
+
 // Middleware that parses the body into json
 app.use(express.json());
 
+//  Doesn't matter which order the body parser
+//  and the logger are called 
+const logger = (req, res, next) => {
+    console.log("host:", req.hostname);
+    console.log("method:", req.method);
+    console.log("path:", req.path);
+    // Stop this and call the next function - one of the request methods
+    // (GET/POST etc.) otherwise execution will freeze 
+    return next();
+};
+// Use our own middleware logger
+app.use(logger);
+
 const cats = [];
+
+// Requests have two major components we are interested in: the path 
+// and the method. The path is the location the request will be sent to (e.g. /getAll)
+//  and the method is the type of request (e.g. GET/POST/PATCH).
+
+// req is an object that represents the received request and can be used to view 
+// sent data and request metadata such as headers, cookies, etc.
 app.get('/getAll', (req, res) => {res.json(cats)});
+
+// Post requests create data
 app.post('/create', (req, res) => {
                         const newCat = req.body; 
                         cats.push(newCat);
@@ -19,13 +42,24 @@ app.delete('/remove/:id', (req, res) => {
                         res.json(removed);
 });
 
-                    
-app.patch('/update/:id', (req, res) => {
+                   
+// Patch - partial update request method
+app.patch('/update/:id', (req, res, next) => {
                         const {id} = req.params;
+                        if(id >= cats.length)
+                            return next({msg:`ID: ${id} out of bounds`, status: 404});
                         const {name} = req.query;
                         const catToUpdate = cats[id];
                         catToUpdate.name = name;
                         res.json(catToUpdate);
+});
+
+// Error handling middleware - called by request methods if needed
+// Need to have next even though we might not use it. But error handler
+// has to have 4 parameters JS can't tell two methods apart if they have same 
+// number of parameters.
+app.use((err, req, res, next) => {
+    res.status(err.status).send(err.msg);
 });
 
 //  In order to communicate with the Express app we need
